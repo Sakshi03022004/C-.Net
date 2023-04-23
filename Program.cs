f36be1532461c3c89043ae8f4bda702461bb4b7b
Sakshi03022004 <99466592+Sakshi03022004@users.noreply.github.com>
2023-04-23 18:35:35 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,19 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace _1_hello_word
+namespace palindromno
 {
+    class demo
+    {
+        int temp, rem, sum, no;
+       public void get()
+        {
+            Console.WriteLine("enter the number");
+            no = Convert.ToInt32(Console.ReadLine());
+        }
+        public void show()
+        {
+            temp = no; 
+            while (no > 0)
+            {
+                rem = no % 10;
+                sum = (sum * 10) + rem;
+                no = no / 10;
+            }
+            if (temp == sum)
+            {
+                Console.WriteLine("given number is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("given number is not palindrome");
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i < 10; i++)
-            {
-                Console.WriteLine("hello word");
-                
-            }
+
+            demo d1=new demo();
+            d1.get();
+            d1.show();
             Console.ReadLine();
         }
-        
     }
 }
